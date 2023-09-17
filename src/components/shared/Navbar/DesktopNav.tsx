92cfdcd3445c536de8b5fbc47587f1cb2de18750
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-09-17 17:29:10 +0600

    folder structure done

---
+++ src/components/shared/Navbar/DesktopNav.tsx
@@ -0,0 +1,24 @@
+
+const DesktopNav = () => {
+    const paths1 = [
+        {
+            page: "Home",
+            route: "/",
+        },
+        {
+            page: "Events",
+            route: "/",
+        },
+        {
+            page: "News",
+            route: "/",
+        }
+    ]
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default DesktopNav;
