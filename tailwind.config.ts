3c07f060f98593ff8948379bbeb5e407c7067802
Mdabdullah3 <mdabdullahnoman777@gmail.com>
2023-09-17 17:07:35 +0600

    package install

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#fb4051",
+          secondary: "#0D0D0D",
+          neutral: "#F3F4F6",
+          "base-100": "#ffffff",
+          info: "#ECF0F3",
+          success: "#521647",
+          warning: "#DF7E07",
+          error: "#FA5C5C",
+        },
       },
-    },
+      {
+        night: {
+          primary: "#11DEDE",
+          secondary: "#ffff",
+          neutral: "#F3F4F6",
+          "base-100": "#0A0F1E",
+          info: "#111827",
+          success: "#1BBB70",
+          warning: "#F59E0B",
+          error: "#FB7185",
+        },
+      },
+    ],
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("daisyui")],
+};
+export default config;
